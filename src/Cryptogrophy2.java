409392270b937ac2e784ca6d81070c9d230f7e79
ahance21 <42975655+ahance21@users.noreply.github.com>
2019-06-05 14:35:52 -0400

    Add files via upload

---
+++ src/Cryptogrophy2.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Cryptogrophy2
+{
+
+	public static void main(String[] args) 
+	{
+	
+		Scanner keyboard = new Scanner(System.in);
+		
+		char[]alpha = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+		   char[]alphaup = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+		System.out.print("Would you like to encrypt or decrypt?");
+        
+		
+		
+
+	}
+	
+	
+	
+	public static String caesar_cipher(String filename, boolean encrypt, int shiftAmount) throws IOException
+	{
+		   Scanner file = new Scanner(new File(filename));
+
+		if (encrypt == true)	
+		{
+			
+			   
+		}
+		return fileName;
+	}
+	private static boolean isUpper(char charAt) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	private static boolean isLower(char charAt) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+}
